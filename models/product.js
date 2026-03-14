1b8fb74edbbc1783af17b6d1a2fdd86c81a5873a
NovaCoreXLabs27 <novacorexlabs27@gmail.com>
2026-03-14 20:27:35 +0100

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
 
+const productSchema = new mongoose.Schema({
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  name: String,
+  description: String,
+  category: String,
+  price: Number,
+  fresh: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Product", productSchema);
