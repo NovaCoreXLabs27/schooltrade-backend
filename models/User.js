5372b548ee2ddc0b7e844ced4e0905ba022b18ed
NovaCoreXLabs27 <novacorexlabs27@gmail.com>
2026-03-14 20:25:47 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  plan: {
+    type: String,
+    default: "basic"
+  },
+  badge: {
+    type: String,
+    default: "bronze"
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
