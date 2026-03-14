ef00337eefbac240315b62aa8aee70f1ba109a67
NovaCoreXLabs27 <novacorexlabs27@gmail.com>
2026-03-14 20:24:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const productRoutes = require("./routes/products");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/products", productRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("SchoolTrade Backend Running");
+});
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("MongoDB Error:", err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
